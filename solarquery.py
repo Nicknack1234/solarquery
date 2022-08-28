5722600f41c4badb1c0ab8db0b9ccce8ae8d08d0
Nicknack1234 <nicknack1234@hotmail.com>
2022-08-28 19:53:45 +0800

    Update solarquery.py

--- solarquery.py
+++ solarquery.py
@@ -3,103 +3,77 @@
 import RS485Ext
 from pymodbus.client.sync import ModbusSerialClient
 from influxdb import InfluxDBClient
+from datetime import datetime, time
 import time
-import requests
 from threading import Thread
-import os
-
-MIN_SIGNED   = -2147483648
-MAX_UNSIGNED =  4294967295
-
-MODEL = os.getenv('model', 'sungrow-sg5d')
-#INVIP = os.getenv('inverter_ip', '')
-#INVPORT = int(os.getenv('inverter_port', 502))
-INVTIMEOUT = int(os.getenv('timeout', 3))
-
-INFIP = os.getenv('influxdb_ip', 'nick.lan')
-INFPORT = int(os.getenv('influxdb_port', 8086))
-INFUSER = os.getenv('influxdb_user', 'grafana')
-INFPASSWORD = os.getenv('influxdb_password', 'grafana')
-INFDATABASE = os.getenv('influxdb_database', 'metrics')
-
-MODID = int(os.getenv('modbus_slave', 0x01))
-MODINTV = int(os.getenv('scan_interval', 10))
 
 
-requests.packages.urllib3.disable_warnings() 
-
-print ("Load modbus map %s" % MODEL)
-
-modmap_file = "modbus-" + MODEL
+print ("Load modbus map sungrow-sg5d")
+modmap_file = "modbus-sungrow-sg5d"
 modmap = __import__(modmap_file)
+flux_client = InfluxDBClient('nick.lan', 8086, 'grafana', 'grafana', 'metrics', ssl=False, verify_ssl=False)
 
-#client = ModbusTcpClient(INVIP, 
-#                         timeout=INVTIMEOUT,
-#                         RetryOnEmpty=True,
-#                         retries=3,
-#                         port=INVPORT)
-ser=RS485Ext.RS485Ext(port='/dev/serial0', baudrate=9600, timeout=5)
-client = ModbusSerialClient(method='rtu')
-client.socket = ser
-client.connect()
 
-try:
-  flux_client = InfluxDBClient(INFIP,
-                               INFPORT,
-                               INFUSER,
-                               INFPASSWORD,
-                               INFDATABASE,
-                               ssl=False,
-                               verify_ssl=False)
-except:
-  flux_client = None
-
-inverter = {}
-
-def publish_influx(metrics):
-  target=flux_client.write_points([metrics])
+def publish_influx(inverter):
+  metrics = {}
+  tags = {}
+  metrics['measurement'] = "Sungrow"
+  tags['location'] = "Home"
+  metrics['tags'] = tags
+  metrics['fields'] = inverter
+  flux_client.write_points([metrics])
   print ("Sent to InfluxDB")
 
-#ping inverter to see if its on?
-while True:
-  time.sleep(MODINTV)
+def in_between(now, start, end):
+  if start <= end:
+      return start <= now < end
+  else:
+      return start <= now or now < end
+
+def validateModbus(client=None):
+  if (client is None) or (client.is_socket_open() == False):
+    print("Client is null or not connected")
+    client = connectModBus()
+  return client
+
+def connectModBus():
+  print("Connecting...")
+  ser=RS485Ext.RS485Ext(port='/dev/serial0', baudrate=9600, timeout=5)
+  client = ModbusSerialClient(method='rtu')
+  client.socket = ser
+  client.connect()
+  return client
+
+def isConnected(client):
   try:
-    inverter = {}
-    print ("Getting Data")
+    rr = client.read_input_registers(5007, count=1, unit=1)
+    if ("Exception" not in str(rr)) and ("Error" not in str(rr) and ("ModbusIOException" not in str(rr))):
+      return True
+    else:
+      return False
+  except:
+    return False
+
+
+while True:
+  print ("Waiting")
+  time.sleep(60)
+  inverter = {}
+  client = validateModbus()
+  if (isConnected(client) == False):
+    print ("Offline, setting data zero")
     for addr in modmap.read_register:
-      rr = client.read_input_registers(int(addr), count=1, unit=MODID)
+      inverter[str(addr)] = 0
+  else:
+    print ("Online, Getting Data")
+    for addr in modmap.read_register:
+      rr = client.read_input_registers(int(addr), count=1, unit=1)
       if ("Exception" not in str(rr)) and ("Error" not in str(rr) and ("ModbusIOException" not in str(rr))):
         inverter[str(addr)] = rr.registers[0]
       else:
         print(str(rr))
 
-    print("Loaded Data from Inverter, Sending to Influx")
-
-    if flux_client is not None:
-      metrics = {}
-      tags = {}
-      fields = {}
-      metrics['measurement'] = "Sungrow"
-      tags['location'] = "Home"
-      metrics['tags'] = tags
-      metrics['fields'] = inverter
-      t = Thread(target=publish_influx, args=(metrics,))
-      t.start()
-
-    print ("Sent to Influx")
-  except Exception as e:
-    for addr in modmap.read_register:
-      inverter[str(addr)] = 0
-    metrics = {}
-    tags = {}
-    fields = {}
-    metrics['measurement'] = "Sungrow"
-    tags['location'] = "Home"
-    metrics['tags'] = tags
-    metrics['fields'] = inverter
-    t = Thread(target=publish_influx, args=(metrics,))
+  if flux_client is not None:
+    t = Thread(target=publish_influx, args=(inverter,))
     t.start()
-    try:
-      print(str(rr))
-    except NameError:
-      print("Exception, Sent zeroed data to Influx assuming inverter offline")
+  print ("----------------------------------------------------------")
